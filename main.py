efab1d0df95f2cc4895d1576fde1d368c7cebc20
Mikel Vuka <mikelv92@gmail.com>
2016-04-02 23:50:06 +0000

    setup nginx

--- main.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'blitz'))
-sys.path.append(os.path.join(os.path.dirname(__file__), 'accounts'))
-sys.path.append('/home/mikel/workspace/djangoEnv/lib/python2.7/site-packages')
