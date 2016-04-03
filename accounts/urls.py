fc7393149eb0fc83c6329ee38801e0c615cdc1d6
Mikel Vuka <mikelv92@gmail.com>
2016-04-03 18:35:58 +0200

    Added verifyToken

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from .views import RegisterView
-
 urlpatterns = [
 	url(r'^register/$', RegisterView.as_view(), name='register'),
+	url(r'^verifyToken/$', 'rest_framework_jwt.views.verify_jwt_token'),
+	url(r'^refreshToken/', 'rest_framework_jwt.views.refresh_jwt_token'),
+
 ]
