8f1a3ea143d3d8844f7632dc8753e92158f47eb5
Mikel Vuka <mikelv92@gmail.com>
2016-04-16 15:05:36 +0200

    modified admin

--- images/admin.py
+++ images/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Image, Topic, Chapter, Blitz
+from .models import UserTopic, Topic, UserChapter, Chapter, Blitz
 
 # Register your models here.
-admin.site.register(Image)
+admin.site.register(UserTopic)
 admin.site.register(Topic)
 admin.site.register(Chapter)
+admin.site.register(UserChapter)
 admin.site.register(Blitz)
