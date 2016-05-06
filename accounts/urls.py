aef40d5fc3930ff93d27e81368a85d5a74a63a52
Mikel Vuka <mikelv92@gmail.com>
2016-05-07 00:03:37 +0200

    Change password view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import RegisterView, AvatarView, ProfileView, ChangeUsernameView
+from .views import RegisterView, AvatarView, ProfileView, ChangeUsernameView, ChangePasswordView
 urlpatterns = [
 	url(r'^register/$', RegisterView.as_view(), name='register'),
 	url(r'^profile/$', ProfileView.as_view(), name='profile'),
@@ -8,4 +8,5 @@ urlpatterns = [
 	url(r'^refreshToken/$', 'rest_framework_jwt.views.refresh_jwt_token'),
 	url(r'^avatar/$', AvatarView.as_view(), name='avatar'),
 	url(r'^changeUsername/$', ChangeUsernameView.as_view(), name='changeUsername'),
+	url(r'^changePassword/$', ChangePasswordView.as_view(), name='changePassword'),
 ]
